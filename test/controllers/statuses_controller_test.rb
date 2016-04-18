76525d9fa8ad134e1192b0853778b6e3083b79e0
Tien Do <domanhtien2011@gmail.com>
2016-04-19 01:39:23 +0700

    add model test for status

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -17,13 +17,20 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
+  test "should be logged in when post a new status" do
+    post :create, status: { content: "Hello" }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should render the new page when logged in" do
     sign_in users(:user1)
     get :new
     assert_response :success
   end
 
-  test "should create status" do
+  test "should create status when logged in" do
+    sign_in users(:user1)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -36,12 +43,26 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test 'should be logged in when edit a status' do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should get edit" do
+    sign_in users(:user1)
     get :edit, id: @status
     assert_response :success
   end
 
+  test "should be logged in when update a status" do
+    patch :update, id: @status, status: { content: @status.content}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
   test "should update status" do
+    sign_in users(:user1)
     patch :update, id: @status, status: { content: @status.content}
     assert_redirected_to status_path(assigns(:status))
   end
